495d885fd8c08e1d70de90fceb69ca89de7e7a2b
Des Flanagan <desflanagan@hotmail.com>
2015-08-27 23:26:49 +1000

    cleanup

--- RiskAnalysis.Services/Services/CustomerService.cs
+++ RiskAnalysis.Services/Services/CustomerService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using AutoMapper;
 using RiskAnalysis.DataAccess.Repository.Interfaces;
 using RiskAnalysis.Domain.Entities;
