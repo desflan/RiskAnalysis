fee882c0b2f1d7b0cf7aecf78cc7bd6829ea30d0
Des Flanagan <desflanagan@hotmail.com>
2015-08-27 20:06:02 +1000

    added csv data migration. Installed CsvHelper

--- RiskAnalysis.DataAccess/Seed/DataMigration.cs
+++ RiskAnalysis.DataAccess/Seed/DataMigration.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using CsvHelper;
 using RiskAnalysis.DataAccess.Models;
 using System.Data.Entity.Migrations;
 using RiskAnalysis.Domain.Entities;
@@ -21,6 +25,8 @@ namespace RiskAnalysis.DataAccess.Seed
         public void InsertData()
         {
             InsertCustomers();
+            InsertSettledData();
+            InsertUnsettledData();
         }
 
         private void InsertCustomers()
@@ -34,5 +40,50 @@ namespace RiskAnalysis.DataAccess.Seed
 
             _context.SaveChanges();
         }
+
+        private void InsertUnsettledData()
+        {
+            string unsettledData = ConfigurationManager.AppSettings["CsvUnsettled"];
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream(unsettledData);
+            if (stream != null)
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    var csvReader = new CsvReader(reader);
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    var data = csvReader.GetRecords<UnsettledBet>().ToArray();
+                    foreach (var unsettled in data)
+                    {
+                        unsettled.Customer = null;
+                    }
+                    _context.UnsettledBets.AddOrUpdate(c => c.Id, data);
+                    _context.SaveChanges();
+                }
+
+        }
+
+        private void InsertSettledData()
+        {
+            var settledData = ConfigurationManager.AppSettings["CsvSettled"];
+
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream(settledData);
+            if (stream != null)
+                using (var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    var csvReader = new CsvReader(reader);
+                    csvReader.Configuration.WillThrowOnMissingField = false;
+                    var data = csvReader.GetRecords<SettledBet>().ToArray();
+                    foreach (var settled in data)
+                    {
+                        settled.Customer = null;
+                    }
+                    _context.SettledBets.AddOrUpdate(c => c.Id, data);
+                    _context.SaveChanges();
+                }
+        }
+
+
     }
 }
